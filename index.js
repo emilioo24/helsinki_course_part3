1ddaf2e9238eb4bb60f5a3558f3a4f2a23197794
Emilioo24 <lobocalderon@outlook.com>
2025-04-24 19:10:31 -0300

    Creando base de datos con MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 app.use(express.static('dist'));
 app.use(cors());
@@ -13,100 +15,117 @@ morgan.token('body', (req, res) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms  :body'));
 
-let agenda = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    },
-    { 
-      "id": 5,
-      "name": "Matias Lopez", 
-      "number": "343 460-6845"
-    }
-];
-
-//muestra la lista total de personas
-app.get('/api/persons', (req, res) => {
-    res.json(agenda);
+//muestra la lista total de personas - GET
+app.get('/api/persons', (req, res, next) => {
+    Person.find({}).then((person) => {
+        res.json(person);
+    })
+    .catch(error => next(error));
 });
 
-//muestra el número total de personas en la agenda y la hora de la solicitud
-app.get('/info', (req, res) => {
-    const sinplu = agenda.length > 1 ? 'people' : 'person';
-    res.send(
-        `<div>
-            <p>Phonebook has info for ${agenda.length} ${sinplu}</p>
-            <p>${new Date()}</p>
-        </div>`
-    )
+//muestra el número total de personas y la hora de la solicitud - GET
+app.get('/info', (req, res, next) => {
+    Person.find({}).then(result => {
+        const sinplu = result.length > 1 ? 'people' : 'person';
+        res.send(
+            `<div>
+                <p>Phonebook has info for ${result.length} ${sinplu}</p>
+                <p>${new Date()}</p>
+            </div>`
+        )
+    })
+    .catch(error => next(error));
 });
 
-//muestra una persona en específico por su id, de no existir devuelve ERROR 404
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const data = agenda.find((person) => person.id === id);
-    if (data) {
-        res.json(data)
-    } else {
-        res.status(404).end();
-    }
+//muestra una persona en específico por su id, de no existir devuelve ERROR 404 - GET
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(error => next(error));
 });
 
-//elimina una persona en específico por su id
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    agenda = agenda.filter(person => person.id !== id);
-    res.status(204).end();
+//elimina una persona en específico por su id - DELETE
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
 //Función que genera un id random entre 0 y 10000000
-const randomId = () => {
+/*const randomId = () => {
     const id = Math.floor(Math.random() * 10000001)
     return id;
-}
+}*/
 
-//añade una persona nueva al array generando un id random
+//añade una persona nueva al array - POST
 app.post('/api/persons', (req, res) => {
     const body = req.body;
-    const verify = agenda.find(person => person.name === body.name);
+    //const verify = Person.find({ name: body.name }).then(name => name);
 
     if (!body.name || !body.number) {
         return res.status(400).json({
             error: 'name or number missing'
         });
-    } else if (verify) {
+    } /*else if (false) {
         return res.status(409).json({
             error: 'name must be unique'
         });
-    }
+    }*/
 
-    const newData = {
-        id: randomId(),
+    const newData = new Person({
+        name: body.name,
+        number: body.number
+    });
+
+    newData.save().then((savedData) => {
+        res.json(savedData);
+    });
+});
+
+//actualiza el número de una persona con el mismo nombre - PUT
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body;
+
+    const data = {
         name: body.name,
         number: body.number
     }
-    agenda = agenda.concat(newData);
-    res.json(newData);
+
+    Person.findByIdAndUpdate(req.params.id, data, { new: true })
+        .then(updatePerson => {
+            res.json(updatePerson);
+        })
+        .catch(error => next(error));
 });
 
+//crea un middleware para las urls no encontradas
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' });
+}
+
+app.use(unknownEndpoint);
+
+//crea un middleware para manejar los errores
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message);
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' });
+    }
+    next(error);
+}
+
+app.use(errorHandler);
+
 //establece el puerto para el servidor
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
---
+++ mongo.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+let print = false;
+let name = '';
+let number = '';
+
+if (process.argv.length === 5) {
+    name = process.argv[3];
+    number = process.argv[4];
+} else if (process.argv.length === 3) {
+    print = true;
+} else {
+    console.log('need more data to process the app');
+    process.exit(1);
+}
+
+const password = encodeURIComponent(process.argv[2]);
+
+const url = `mongodb+srv://emilianogstroke:${password}@cluster0.y7lciof.mongodb.net/PhonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set('strictQuery',false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: name,
+  number: number
+});
+
+if (print) {
+    Person.find({  }).then(result => {
+        console.log('phonebook:');
+        result.forEach(person => {
+          console.log(person.name, person.number);
+        })
+        mongoose.connection.close();
+    });
+} else {
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`);
+        mongoose.connection.close();
+    });
+}
